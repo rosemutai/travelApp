0bb700e92cced10facae50ead8c562f7bd8cc85d
rosemutai <chepngetichrose2030@gmail.com>
2022-05-20 19:54:15 +0300

    register user

--- backend/index/views.py
+++ backend/index/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import authentication
+from rest_framework.permissions import AllowAny
+
+from .serializers import RegisterSerializer
+
 
 
 # Create your views here
+class RegisterUserApiView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+            
