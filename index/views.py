3f1a3e8b4813e37ba82aab6b765ca0599b8140ed
rosemutai <chepngetichrose2030@gmail.com>
2022-05-21 11:28:39 +0300

    user register vie

--- index/views.py
+++ index/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+
+from .serializers import RegisterSerializer
 
 # Create your views here.
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny,]
+    serializer_class = RegisterSerializer
+    
