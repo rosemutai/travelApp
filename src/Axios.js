8bcb68825b7dcc44777e2253dd47b97d279a3322
rosemutai <chepngetichrose2030@gmail.com>
2022-05-24 21:34:12 +0300

    axios config

---
+++ src/Axios.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+
+const baseURL = 'http://localhost:8000/auth/'
+
+const axiosInstance = axios.create({
+    baseURL: baseURL,
+    timeout: 5000,
+    headers: {
+        Authorization: localStorage.getItem('access_token')
+            ? 'JWT ' + localStorage.getItem('access_token')
+            : null,
+        'Content-Type': 'application-json',
+        accept: 'application-json'
+
+    },
+})
+
+export default axiosInstance
