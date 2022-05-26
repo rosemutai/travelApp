6b590f83f39222ada302e042c51ad3a7baa82e45
rosemutai <chepngetichrose2030@gmail.com>
2022-05-26 21:00:12 +0300

    simple jwt

--- backend/settings.py
+++ backend/settings.py
@@ -119,7 +119,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
     'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
