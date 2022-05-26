9dbd1a77c08658216c86bb779e4175f3c48cd332
rosemutai <chepngetichrose2030@gmail.com>
2022-05-26 21:00:53 +0300

    added profile route

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './index.css'
 import Home from './components/Home'
 import SignUpForm from './routes/SignUpForm';
 import LoginForm from './routes/LoginForm';
+import LogOut from './routes/LogOut';
 import Explore from './routes/Explore'
 import AboutUs from './routes/AboutUs';
 import Contact from './routes/Contact';
+import Profile from './routes/Profile';
 
 function App() {
   return (
@@ -15,9 +17,11 @@ function App() {
           <Route path='/safiriapp' element={<Home/>} ></Route>
           <Route path='/signup' element={<SignUpForm/>}></Route>
           <Route path='/login' element={<LoginForm/>}></Route>
+          <Route path='/logout' element={<LogOut />}></Route>
           <Route path='/explore' element={<Explore />}></Route>
           <Route path='/about' element={<AboutUs />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
           
         </Routes>
       </BrowserRouter>
