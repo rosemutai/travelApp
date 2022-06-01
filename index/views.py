a6261255283bbc3484b8fd304bd38c412178d89f
rosemutai <chepngetichrose2030@gmail.com>
2022-06-01 13:52:09 +0300

    logout view

--- index/views.py
+++ index/views.py
@@ -36,4 +36,4 @@ class LogoutApiView(APIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-    
+
