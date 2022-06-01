0a4eb28cc2604a3d3594220116c518b661534111
rosemutai <chepngetichrose2030@gmail.com>
2022-06-01 19:11:58 +0300

    added logout dispatch

--- src/routes/LogOut.js
+++ src/routes/LogOut.js
@@ -1,19 +1,26 @@
-import {React, useEffect} from 'react'
+import {React, useEffect, useContext} from 'react'
 import axiosInstance from '../Axios'
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../App'
 
 function LogOut() {
 
     const navigate = useNavigate()
+    const { dispatch } = useContext(AuthContext)
+   
     useEffect(() => {
-        const response = axiosInstance.post('logout/',{
+        axiosInstance.post('logout/',{
             refresh_token: localStorage.getItem('refresh_token'),
+           
         })
-        localStorage.removeItem('access_token');
-        localStorage.removeItem('refresh_token');
-        axiosInstance.defaults.headers['Authorization'] = null;
-        navigate('/login')
-
+        .then(res =>{
+            console.log(res)
+            dispatch({
+                type: "LOGOUT",
+                payload: res.data
+            })
+            navigate('/login')
+        })
 
     })
     return (
@@ -24,3 +31,33 @@ function LogOut() {
 }
 
 export default LogOut
+
+
+
+
+
+
+
+
+
+
+// function LogOut() {
+
+//     const navigate = useNavigate()
+//     useEffect(() => {
+//         const response = axiosInstance.post('logout/',{
+//             refresh_token: localStorage.getItem('refresh_token'),
+//         })
+//         localStorage.removeItem('access_token');
+//         localStorage.removeItem('refresh_token');
+//         axiosInstance.defaults.headers['Authorization'] = null;
+//         navigate('/login')
+
+
+//     })
+//     return (
+//         <div>
+//             <h2>Logout</h2>
+//         </div>
+//     )
+// }
